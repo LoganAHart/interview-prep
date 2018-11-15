7338fbb74289df9d84d1476f8ba9785d9121f3f8
Logan Hart <2loganhart@gmail.com>
2018-11-14 20:04:18 -0800

    implement binary search recursive and iterative, add basic jest tests

--- arrays-and-strings/binary-search/index.js
+++ arrays-and-strings/binary-search/index.js
@@ -1,5 +1,34 @@
-const func = (args) => {
-  return 'answer'
+const bsRecursive = (arr, key, low = 0, high = arr.length - 1) => {
+  const mid = Math.floor((low + high) / 2)
+  if (arr[mid] === key) {
+    return mid
+  } else if (low > high) {
+    return -1
+  } else if (arr[mid] > key) {
+    return bsRecursive(arr, key, low, mid - 1)
+  } else {
+    return bsRecursive(arr, key, mid + 1, high)
+  }
 }
 
-module.exports = { func }
+const bsIterative = (arr, key) => {
+  let low = 0
+  let high = arr.length - 1
+  let mid
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2)
+    if (arr[mid] === key) {
+      return mid
+    } else if (arr[mid] > key) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+  return -1
+}
+
+module.exports = {
+  bsRecursive,
+  bsIterative
+}
--- arrays-and-strings/binary-search/test.js
+++ arrays-and-strings/binary-search/test.js
@@ -1,5 +1,30 @@
-const { func } = require('./index')
+const {
+  bsRecursive,
+  bsIterative
+} = require('./index')
 
-test('func(args)', () => {
-  expect(func('args')).toEqual('answer')
+const testArr = [10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155]
+
+test(`bsRecursive([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 47)`, () => {
+  expect(bsRecursive(testArr, 47)).toEqual(2)
+})
+
+test(`bsRecursive([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 75)`, () => {
+  expect(bsRecursive(testArr, 75)).toEqual(5)
+})
+
+test(`bsRecursive([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 175)`, () => {
+  expect(bsRecursive(testArr, 175)).toEqual(-1)
+})
+
+test(`bsIterative([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 47)`, () => {
+  expect(bsIterative(testArr, 47)).toEqual(2)
+})
+
+test(`bsIterative([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 75)`, () => {
+  expect(bsIterative(testArr, 75)).toEqual(5)
+})
+
+test(`bsIterative([10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155], 175)`, () => {
+  expect(bsIterative(testArr, 175)).toEqual(-1)
 })
