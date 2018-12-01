c7bcb2976da115739f775a35010af2df9f179ea9
Logan Hart <2loganhart@gmail.com>
2018-11-30 17:46:58 -0800

    add new section on css - just covering random demos and interesting tricks for now

--- arrays-and-strings/rotate-matrix/index.js
+++ arrays-and-strings/rotate-matrix/index.js
@@ -1,5 +1,27 @@
-const func = (args) => {
-  return 'answer'
+const rotateImage = (matrix) => {
+  if (matrix.length === 0 || matrix.length !== matrix[0].length) {
+    return false
+  }
+  let n = matrix.length
+  for (let layer = 0; layer < n / 2; layer++) {
+    let first = layer
+    let last = n - 1 - layer
+    for (let i = first; i < last; i++) {
+      let offset = i - first
+      // save top
+      let top = matrix[first][i]
+      // first -> top
+      matrix[first][i] = matrix[last - offset][first]
+      // bottom -> left
+      matrix[last - offset][first] = matrix[last][last - offset]
+      // right -> bottom
+      matrix[last][last - offset] = matrix[i][last]
+      // top -> right
+      matrix[i][last] = top
+      // right < - saved top
+    }
+  }
+  return matrix
 }
 
-module.exports = { func }
+module.exports = { rotateImage }
--- arrays-and-strings/rotate-matrix/test.js
+++ arrays-and-strings/rotate-matrix/test.js
@@ -1,5 +1,35 @@
-const { func } = require('./index')
+const { rotateImage } = require('./index')
 
-test('func(args)', () => {
-  expect(func('args')).toEqual('answer')
+test('rotateImage([[1, 2, 3],[4, 5, 6],[7, 8, 9]])', () => {
+  expect(rotateImage(
+    [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+  )).toEqual(
+    [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ]
+  )
+})
+
+test('rotateImage(args)', () => {
+  expect(rotateImage(
+    [
+      [5, 1, 9, 11],
+      [2, 4, 8, 10],
+      [13, 3, 6, 7],
+      [15, 14, 12, 16]
+    ]
+  )).toEqual(
+    [
+      [15, 13, 2, 5],
+      [14, 3, 4, 1],
+      [12, 6, 8, 9],
+      [16, 7, 10, 11]
+    ]
+  )
 })
